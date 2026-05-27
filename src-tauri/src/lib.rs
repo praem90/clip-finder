e2b32dc5ac33195f4ebdd35e876be920cee91528
Mohan Raj <praem1990@gmail.com>
2026-05-27 12:28:24 +0530

    Pack engine with tauri

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,6 @@
+use tauri::Manager;
+use tauri_plugin_shell::ShellExt;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,9 +10,27 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_drag::init())
+        .setup(|app| {
+            let api_path = app
+                .path()
+                .resource_dir()
+                .unwrap()
+                .join("engine")
+                .join("engine");
+
+            println!("API Path: {:?}", api_path);
+
+            let (mut _rx, _child) = app
+                .shell()
+                .command(api_path.to_str().unwrap())
+                .spawn()
+                .expect("Failed to spawn the process");
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
